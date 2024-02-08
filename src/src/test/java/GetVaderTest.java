affa5fee21a64965ac6a44852aaef390d8c6f1c8
niksami <niksami@gmail.com>
2024-02-09 00:09:51 +0100

    Added additional assert in Vader test to make sure Darth Vader is found!

--- src/src/test/java/GetVaderTest.java
+++ src/src/test/java/GetVaderTest.java
@@ -1,7 +1,7 @@
 import org.example.retrofit.mappings.responses.GetPeopleResponse;
+import org.example.retrofit.mappings.responses.PeopleBean;
 import org.junit.jupiter.api.Test;
 import retrofit2.Response;
-
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.example.retrofit.endpointshelper.GetPeopleEndpoints.getPeopleResponse;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -11,7 +11,12 @@ public class GetVaderTest {
     @Test
     public void getVaderTest() {
         Response<GetPeopleResponse> getResponse = getPeopleResponse("Vader");
+        assert getResponse.body() != null;
+        PeopleBean[] peopleFound = getResponse.body().getResults();
         assertThat("Endpoint works!", getResponse.code(), is(SC_OK));
+        for (PeopleBean peopleBean : peopleFound) {
+            assertThat("Searching for Vader...", peopleBean.getName(), is("Darth Vader"));
+        }
     }
 
 }
